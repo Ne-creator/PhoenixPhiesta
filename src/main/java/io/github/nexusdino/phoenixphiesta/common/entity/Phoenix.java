34461aedda6e237a5442a1acc0d56e3dcf1fba2e
NexusDino <86781290+Nexus-Dino@users.noreply.github.com>
2022-07-16 21:59:01 +0100

    fill implementation of quickMoveStack in the menu

---
+++ src/main/java/io/github/nexusdino/phoenixphiesta/common/entity/Phoenix.java
@@ -0,0 +1,11 @@
+package io.github.nexusdino.phoenixphiesta.common.entity;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.FlyingMob;
+import net.minecraft.world.level.Level;
+
+public class Phoenix extends FlyingMob {
+    protected Phoenix(EntityType<? extends FlyingMob> p_20806_, Level p_20807_) {
+        super(p_20806_, p_20807_);
+    }
+}
